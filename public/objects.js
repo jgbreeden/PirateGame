a9372e5d1163acb10c06315ad928ca3c57893d8a
unknown <damianalicea03@gmail.com>
2020-10-28 17:17:10 -0700

    added Function Bullet

--- public/objects.js
+++ public/objects.js
@@ -61,16 +61,24 @@ function PlayerShip(x, y, dir, a){//the place the players spawn,
 		} else if (this.dir == 315){//NE or NE
 			this.y -= Move.dis;
 			this.x += Move.dis;
+			//CheckBounds(PlayerShip)
+
 		};
 	};
 	this.fire = function(Fire){//make thing shoot thing
-
+		//Create Projectile
+		//Emit Message
 
 	};
 };
 	
 
-
+function Bullet(x, y){
+	this.x = x;
+	this.y = y;
+	this.start.x
+	this.start.y
+}
 function BountyShip(x, y, dir, a){// Ship.AI, moves w/no limitation 
 	this.x = x;
 	this.y = y;
