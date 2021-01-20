02ce30afade62a10143f2af19a355403d8645598
Daniel Bush <danielbush7104@gmail.com>
2021-01-20 15:31:30 -0700

    fixed css

--- db.config.js
+++ db.config.js
@@ -1,6 +1,8 @@
 module.exports = {
      HOST: "us-cdbr-east-03.cleardb.com",
-     USER: "bad1e99640bc3c",
-     PASSWORD: "dca510f4",
-     DB: "heroku_e9f1acb2e8b64f8"
+     USER: "bb33eaa2ed08bb",
+     PASSWORD: "8f519c32",
+     DB: "heroku_978bc50cf3a7547"
    };
+
+//mysql --host=us-cdbr-east-03.cleardb.com --user=bb33eaa2ed08bb --password=8f519c32 --reconnect heroku_978bc50cf3a7547
